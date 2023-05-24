d3ad0af17bb5efce097aceb3ebcba017fa0e22cb
Joshua Lehmann <joshua.le1999@gmail.com>
2023-05-24 17:22:55 +0200

    Added entities, repositories and dto and also open-api and swagger-ui

--- src/main/java/hftm/joshua/service/BlogService.java
+++ src/main/java/hftm/joshua/service/BlogService.java
@@ -1,6 +1,9 @@
 package hftm.joshua.service;
 
+import hftm.joshua.data.Author;
 import hftm.joshua.data.Blog;
+import hftm.joshua.dto.BlogRequest;
+import hftm.joshua.repository.AuthorRepository;
 import hftm.joshua.repository.BlogRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -14,11 +17,28 @@ public class BlogService {
     @Inject
     BlogRepository blogRepository;
     @Inject
+    AuthorRepository authorRepository;
+    @Inject
     Logger logger;
 
     public List<Blog> getAllBlogs() {
         logger.info("Getting all blogs");
-        return blogRepository.getAllBlogs();
+        return blogRepository.listAll();
+    }
+
+    public void addBlog(BlogRequest blogRequest) {
+        Blog blog = new Blog(blogRequest.getTitle(), blogRequest.getContent());
+
+        if (blogRequest.getAuthorId() != null) {
+            Author author = authorRepository.findById(blogRequest.getAuthorId());
+            authorRepository.persist(author);
+            blog.setAuthor(author);
+        }
+        blogRepository.persist(blog);
+    }
+
+    public Blog getBlog(Long id) {
+        return blogRepository.findById(id);
     }
 
 }
